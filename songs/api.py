0a1ef7c43edca9e130c79f880f48b71130250717
takutico <takutico@yahoo.es>
2016-12-02 15:48:28 +1100

    customized returning data in json

--- songs/api.py
+++ songs/api.py
@@ -5,5 +5,7 @@ from songs.models import Song
 
 class SongResource(ModelResource):
     class Meta:
+        fields = ['title', 'author', 'link']
         queryset = Song.objects.all().order_by('-created')
         resource_name = 'song'
+        include_resource_uri = False
