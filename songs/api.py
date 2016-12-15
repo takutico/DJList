7dd0fecbb94d555afba95c34dfa38938f5e1a1ac
takutico <takutico@yahoo.es>
2016-12-15 14:12:12 +1100

    add created to api, the first ordering should be by created field in descending

--- songs/api.py
+++ songs/api.py
@@ -6,8 +6,9 @@ from songs.models import Song
 
 class SongResource(ModelResource):
     class Meta:
-        fields = ['title', 'author', 'link']
+        fields = ['created', 'title', 'author', 'link']
         queryset = Song.objects.all().order_by('-created')
         resource_name = 'song'
         include_resource_uri = False
         authorization = Authorization()
+        always_return_data = True
